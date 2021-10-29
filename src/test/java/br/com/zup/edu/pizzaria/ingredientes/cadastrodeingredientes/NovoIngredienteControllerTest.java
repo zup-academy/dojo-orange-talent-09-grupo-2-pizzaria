fc732167709529f114839e5e438f3a266c42f65c
Diogo Magliano <diogo.magliano@zup.com.br>
2021-10-29 10:02:03 -0300

    piloto diogo

--- src/test/java/br/com/zup/edu/pizzaria/ingredientes/cadastrodeingredientes/NovoIngredienteControllerTest.java
+++ src/test/java/br/com/zup/edu/pizzaria/ingredientes/cadastrodeingredientes/NovoIngredienteControllerTest.java
@@ -1,33 +1,32 @@
 package br.com.zup.edu.pizzaria.ingredientes.cadastrodeingredientes;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import br.com.zup.edu.pizzaria.ingredientes.Ingrediente;
+import br.com.zup.edu.pizzaria.ingredientes.IngredienteRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import javax.transaction.Transactional;
-
 import java.math.BigDecimal;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
 class NovoIngredienteControllerTest {
 
+    @Autowired
+    private IngredienteRepository ingredienteRepository;
 
     @Autowired
     private MockMvc mvc;
@@ -37,13 +36,33 @@ class NovoIngredienteControllerTest {
 
         NovoIngredienteRequest body = new NovoIngredienteRequest("Queijo muçarela", new BigDecimal("2.0"), 200);
         MockHttpServletRequestBuilder request = post("/api/ingredientes")
-                                                .contentType(MediaType.APPLICATION_JSON)
-                                                .content(new ObjectMapper().writeValueAsString(body));
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(body));
 
         mvc.perform(request)
-           .andExpect(status().isCreated())
-           .andExpect(header().exists("Location"))
-                .andExpect(redirectedUrlPattern("/api/ingredientes/\\d"));
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"))
+                .andExpect(redirectedUrlPattern("/api/ingredientes/{id}"));
+
+    }
+
+    @Test
+    void deveRetornar400CasoIngredienteRepetido() {
+
+        Ingrediente ingrediente = new Ingrediente(
+                "Queijo muçarela",
+                200,
+                new BigDecimal("2.0"));
+
+        NovoIngredienteRequest body = new NovoIngredienteRequest("Queijo muçarela", new BigDecimal("2.0"), 200);
+        MockHttpServletRequestBuilder request = post("/api/ingredientes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(body));
+
+        mvc.perform(request)
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
 
     }
 }
